29df707b7e2b26a4acb6508fc16e1608f12206df
hjpark <hjpark@accupix.com>
2019-04-29 18:37:30 +0800

    Init linux-rga-demo

---
+++ rgaRotation/rgaRotation.d
@@ -0,0 +1,11 @@
+/home/firefly/works/linux-rga-demo/demo/rgaRotation/rgaRotation.o: \
+ /home/firefly/works/linux-rga-demo/demo/rgaRotation/rgaRotation.cpp \
+ /opt/librga_linux/include/RockchipRga.h \
+ /opt/librga_linux/include/drmrga.h /opt/librga_linux/include/rga.h \
+ /opt/librga_linux/include/RockchipRgaMacro.h \
+ /opt/librga_linux/include/RockchipFileOps.h
+ /home/firefly/works/linux-rga-demo/demo/rgaRotation/rgaRotation.cpp :
+ /opt/librga_linux/include/RockchipRga.h :
+ /opt/librga_linux/include/drmrga.h /opt/librga_linux/include/rga.h :
+ /opt/librga_linux/include/RockchipRgaMacro.h :
+ /opt/librga_linux/include/RockchipFileOps.h :
---
+++ rgaSlt/rgaSlt.d
@@ -0,0 +1,6 @@
+/opt/demo/rgaSlt/rgaSlt.o: /opt/demo/rgaSlt/rgaSlt.cpp /opt/librga_linux//RockchipRga.h \
+ /opt/librga_linux//drmrga.h /opt/librga_linux//rga.h \
+ /opt/librga_linux//RockchipFileOps.h
+/opt/demo/rgaSlt/rgaSlt.cpp /opt/librga_linux//RockchipRga.h :
+ /opt/librga_linux//drmrga.h /opt/librga_linux//rga.h :
+ /opt/librga_linux//RockchipFileOps.h :
